1d8bd45c7445e2788a594d9e50992448e53d09c2
Ahmad AlSalama <ahmad.alsalama@gmail.com>
2013-03-26 18:34:42 +0300

    create models

--- projectcircle/circleight/models.py
+++ projectcircle/circleight/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Comment(models.Model):
+    name = models.CharField(max_length=200)
+    userid = models.IntegerField()
+    comment = models.TextField()
+    
+    def __unicode__(self):
+        return self.name
 # Create your models here.
