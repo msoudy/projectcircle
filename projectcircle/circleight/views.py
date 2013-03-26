513873ab983405b6959d1b960e495d6d42d714e7
Mohamed Soudy <msoudy7@gmail.com>
2013-03-26 18:47:52 +0300

    views and template

--- projectcircle/circleight/views.py
+++ projectcircle/circleight/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotFound
+from django.shortcuts import render_to_response, HttpResponseRedirect, redirect
+
+
+def index(request):
+	return render_to_response ('circleight/index.html')
