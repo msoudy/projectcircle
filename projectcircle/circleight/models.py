3b63c0269fceee134474a7bfddcb2f5d89b1aced
Ahmad AlSalama <ahmad.alsalama@gmail.com>
2013-03-27 02:12:33 +0300

    update to models
    add comment and user model

--- projectcircle/circleight/models.py
+++ projectcircle/circleight/models.py
@@ -1,10 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Comment(models.Model):
-    name = models.CharField(max_length=200)
-    userid = models.IntegerField()
-    comment = models.TextField()
-    
-    def __unicode__(self):
-        return self.name
-# Create your models here.
+	username = models.CharField(max_length=30) # unique
+ 	postid = models.IntegerField()
+ 	comment = models.TextField()
+
+ 	def __unicode__(self):
+ 		return self.username
+
+
+class User(models.Model):
+	fname = models.CharField(max_length=200) # first name
+	lname = models.CharField(max_length=200) # last name
+	username = models.CharField(max_length=30) # unique
+	posts = models.CommaSeparatedIntegerField(max_length=1000) # The list of posts (by postid)
+
