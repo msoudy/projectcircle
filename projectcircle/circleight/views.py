3fb2fea5958401c598b9ec31a31d080f2f461ebc
Ahmad AlSalama <ahmad.alsalama@gmail.com>
2013-03-27 04:22:34 +0300

    update to database and views
    edited unique and primary keys in the database

--- projectcircle/circleight/views.py
+++ projectcircle/circleight/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render_to_response, HttpResponseRedirect, redirect
 
 def index(request):
 	return render_to_response ('circleight/index.html')
+
+# returns an array of 
+def getUserPosts(request, username):
+	return 1
