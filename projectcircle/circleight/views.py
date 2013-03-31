4825d22b3d850b5f600661dd68012da92dd29367
Mohamed Soudy <msoudy7@gmail.com>
2013-03-31 14:37:32 +0300

    home page edited, still incomplete, css, js
    Model edits:
    
    user points to 8 circlemates with pics
    user points to interests array of 8 with pics
    user points to events array of 8 with pics

--- projectcircle/circleight/views.py
+++ projectcircle/circleight/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render_to_response, HttpResponseRedirect, redirect
 
 
 def index(request):
+	# return circleight mates array of 8 users with pics
+	# return interests array of 8 with pics
+	# events array of 8 with pics
 	return render_to_response ('circleight/index.html')
 
 # returns an array of 
